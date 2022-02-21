47a1303d7c52ee9ddf0bda67b27f841ed689f345
xiaodong.jin <xiaodong.jin@qiyuesuo.com>
2022-02-21 10:00:25 +0800

    feat(Home): 创建HomeActivity

---
+++ home/src/main/java/com/jarvis/home/HomeActivity.kt
@@ -0,0 +1,24 @@
+package com.jarvis.home
+
+import com.jarvis.home.databinding.ActivityHomeBinding
+import com.jarvis.libbase.base.BaseActivity
+
+/**
+ * @author jinxiaodong
+ * @description：
+ * @date 2022/2/21
+ */
+class HomeActivity : BaseActivity<ActivityHomeBinding>() {
+
+
+    override fun getContentLayout() = R.layout.activity_home
+
+    override fun initConfig() {
+    }
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+}
